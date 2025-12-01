92a38c7faa80132ead9aba2373a172506eb5ae81
Cesar Augusto <114227831+cesarbtakeda@users.noreply.github.com>
2025-12-01 14:00:01 -0300

    Update kali-start.bat

--- kali-start.bat
+++ kali-start.bat
@@ -1,6 +1,9 @@
 @echo off
 SET VM_NAME="kali-linux-2025.3-virtualbox-amd64"
 SET DIR_VM="C:\Program Files\Oracle\VirtualBox\VBoxManage.exe"
+SET IP="Defina-seu-IP"
+SET USR="Defina o seu user"
+SET RSA="C:\Users\LIGA\Documents\Kali-start\rsa-kali"
 
 REM Inicia a máquina virtual
 echo Iniciando a maquina virtual Kali Linux...
@@ -11,4 +14,4 @@ echo Aguardando 60 segundos para inicializacao...
 timeout /t 60 /nobreak 
 
 REM Conecta via SSH
-ssh -i "Dir" user@IP
+ssh -i %RSA% %USR%@%IP%
